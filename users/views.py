b575af2ca105e44e24a6e845acbb05a9634270d2
Sneha-Kadam10 <kadamsneha577@gmail.com>
2025-09-01 11:03:38 +0530

    Initial commit of the LMS project

---
+++ users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login
+from .forms import RegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect('course_list')
+        else:
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'users/register.html', {'form': form})
